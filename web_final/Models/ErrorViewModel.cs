1a37259c75c6434a361aba7f0d0f2fec29bd5cea
吳哲文文 <@>
2025-05-02 14:35:58 +0800

    加入專案檔案。

---
+++ web_final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web_final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
